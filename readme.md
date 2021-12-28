33e39363311224e97a9906cd7c78e3febe47dd17
Greenfrogs <5961364+greenfrogs@users.noreply.github.com>
2021-12-28 14:39:30 +0000

    github: init

---
+++ readme.md
@@ -0,0 +1,4 @@
+Deep Learning Plus
+=======================================
+**Deep Learning Plus** is a collection of Python packages designed for the Python 3.9 (with torch==1.7.1, torchvision==0.8.2, torchaudio==0.7.2) for when you only want to have a single import that still works!
+
---
+++ setup.py
@@ -0,0 +1,32 @@
+import pathlib
+
+import pkg_resources
+import setuptools
+
+with open('readme.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='Deep Learning Plus',
+    version='0.0.1',
+    author='Greenfrogs',
+    author_email='5961364+greenfrogs@users.noreply.github.com',
+    description='A collection of Python packages designed for the Python 3.9 for when you only want 1 import',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/greenfrogs/DeepLearningPlus',
+    license='Apache License 2.0',
+    install_requires=[
+        'tqdm==4.62.3',
+        'h5py==3.6.0',
+        'matplotlib==3.5.1',
+        'pytorch-lightning==1.5.7',
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='==3.9',
+)
